89d40c6f754e8eccf98cf43f9b2d76fe7d68f21c
pautovartem <pautov.artem1998@gmail.com>
2018-05-12 11:27:43 +0300

    Fixed Controllers

---
+++ WorkBase/Controllers/ResumesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LogicLayer.Interfaces;
+using LogicLayer.DTO;
+
+namespace WorkBase.Controllers
+{
+    [Authorize]
+    public class ResumesController : ApiController
+    {
+        IResumeService resumeService;
+
+        public ResumesController(IResumeService resumeService)
+        {
+            this.resumeService = resumeService;
+        }
+
+        // GET api/<controller>
+        public IEnumerable<ResumeDTO> Get()
+        {
+            return resumeService.GetAllResumes();
+        }
+
+        // GET api/<controller>/5
+        public ResumeDTO Get(int id)
+        {
+            return resumeService.GetResumeById(id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WorkBase/Controllers/RubricsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LogicLayer.Interfaces;
+using LogicLayer.DTO;
+
+namespace WorkBase.Controllers
+{
+    [Authorize]
+    public class RubricsController : ApiController
+    {
+        readonly IRubricService rubricService;
+
+        public RubricsController(IRubricService rubricService)
+        {
+            this.rubricService = rubricService;
+        }
+
+        // GET api/<controller>
+        public IEnumerable<RubricDTO> Get()
+        {
+            return rubricService.GetAllRubrics();
+        }
+
+        // GET api/<controller>/5
+        public RubricDTO Get(int id)
+        {
+            return rubricService.GetRubricById(id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
