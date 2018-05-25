0894ab5f5fd46c9cea6e6f00c642d16436d55bfb
atmospherria <38794133+atmospherria@users.noreply.github.com>
2018-05-25 21:36:21 +0300

    посмотрите

---
+++ WorkBase.Tests/LogicTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkBase.Tests.LogicTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
