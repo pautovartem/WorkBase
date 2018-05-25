d124eb3d026024758d9f79ee29f42de64199db9a
pautovartem <pautov.artem1998@gmail.com>
2018-05-26 00:33:05 +0300

    Fix AutoMapper

--- WorkBase/Global.asax.cs
+++ WorkBase/Global.asax.cs
@@ -6,8 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using LogicLayer.Infrastructure;
 using AutoMapper;
+using WorkBase.App_Start;
 
 namespace WorkBase
 {
