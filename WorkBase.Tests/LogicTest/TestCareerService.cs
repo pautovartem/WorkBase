cc1c583b9b9616bae3dae368aa12680d3bab4d9e
atmospherria <38794133+atmospherria@users.noreply.github.com>
2018-05-26 13:20:43 +0300

    TestCareerService переделан уже все работает, нужно протестировать edit метод.

--- WorkBase.Tests/LogicTest/TestCareerService.cs
+++ WorkBase.Tests/LogicTest/TestCareerService.cs
@@ -51,18 +51,8 @@ namespace WorkBase.Tests.LogicTest
             careerRepository.Verify(x => x.Create(It.IsAny<Career>()));
         }
 
-
         [Test]
-        public void GetCareer_TryToGetNullValue_ShouldThrow()
-        {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns<Career>(null);
-
-            // act & assert
-            NUnit.Framework.Assert.IsNull(carService.GetCareerById(It.IsAny<int>()));
-        }
-        
-        [Test]
-        public void GetCareer_TryToGetValue_ShouldReturnSomeValue()
+        public void GetCareerById_TryToGetValue_ShouldReturnSomeValue()
         {
             var Career = new Career { Id= It.IsAny<int>()};
 
@@ -71,8 +61,7 @@ namespace WorkBase.Tests.LogicTest
             // act & assert
             NUnit.Framework.Assert.IsNotNull(carService.GetCareerById(It.IsAny<int>()));
         }
-        /*
-        [Test]
+       /* [Test] //нужно реализовать
         public void EditCareer_TryToEditNullElement_ShouldThrow()
         {
             var Career = new CareerDTO { Id = It.IsAny<int>()};
@@ -82,100 +71,30 @@ namespace WorkBase.Tests.LogicTest
             NUnit.Framework.Assert.Throws<ArgumentNullException>(() => carService.EditCareer(Career));
         }
         
-        [Test]
-        public void EditCareer_TryToEditVerifiedCareer_ShouldThrow()
-        {
-            var Career = new CareerDTO { Id = It.IsAny<int>() };
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Id= It.IsAny<int>() });
-
-            //act & assert
-            NUnit.Framework.Assert.Throws<AuctionException>(() => CareerService.EditCareer(Career));
-            Assert.AreEqual(Assert.Throws<AuctionException>(() => CareerService.EditCareer(Career)).Message, "You can`t change the information about the Career after the start of the bidding");
-        }
-        
+      */
         [Test]
         public void EditCareer_EditCareer_ShoudRepositoryEditOnce()
         {
-            var Career = new CareerDTO { Name = It.IsAny<string>(), Price = It.IsAny<double>(), TradeDuration = It.IsAny<int>() };
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Name = It.IsAny<string>(), Price = It.IsAny<double>(), TradeDuration = It.IsAny<int>() });
+            var Career = new CareerDTO { Id = It.IsAny<int>(), ContactName = It.IsAny<string>(), ContactPhone = It.IsAny<string>() };
+            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Id = It.IsAny<int>(), ContactName = It.IsAny<string>(), ContactPhone = It.IsAny<string>() });
 
             //act
-            CareerService.EditCareer(Career);
+            carService.EditCareer(Career);
 
             //assert
             careerRepository.Verify(x => x.Update(It.IsAny<Career>()), Times.Once);
-        }
-
-        [Test]
-        public void DeleteCareer_DeleteNullValue()
-        {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns<Career>(null);
-
-            //act & assert
-            Assert.Throws<ArgumentNullException>(() => CareerService.RemoveCareer(It.IsAny<int>()));
         }
 
         [Test]
         public void DeleteCareer_DeleteRepositoryShouldCallsOnce()
         {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Name = It.IsAny<string>(), Price = It.IsAny<double>(), TradeDuration = It.IsAny<int>() });
+            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Id = It.IsAny<int>(), ContactName = It.IsAny<string>(), ContactPhone = It.IsAny<string>() });
 
             //act
-            CareerService.RemoveCareer(It.IsAny<int>());
+            carService.RemoveCareer(It.IsAny<int>());
 
             //assert
             careerRepository.Verify(x => x.Delete(It.IsAny<int>()));
-        }
-
-        [Test]
-        public void VarifyCareer_TryToVarifyNullCareer_ShouldThrow()
-        {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns<Career>(null);
-
-            //act & assert
-            Assert.Throws<ArgumentNullException>(() => CareerService.VerifyCareer(It.IsAny<int>()));
-        }
-
-        [Test]
-        public void VarifyCareer_TryToVirifySomeCareer_ShouldCallOnce()
-        {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Name = It.IsAny<string>(), Price = It.IsAny<double>(), TradeDuration = It.IsAny<int>() });
-
-            //act 
-            CareerService.VerifyCareer(It.IsAny<int>());
-
-            //assert
-            careerRepository.Verify(x => x.Update(It.IsAny<Career>()), Times.Once);
-        }
-
-        [Test]
-        public void ChangeCareerCategory_TryToChangeWithNullCategory_ShouldThrow()
-        {
-            uow.Setup(x => x.Categories.Get(It.IsAny<int>())).Returns<Category>(null);
-
-            //act & assert
-            Assert.Throws<ArgumentNullException>(() => CareerService.ChangeCareerCategory(It.IsAny<int>(), It.IsAny<int>()));
-        }
-
-        [Test]
-        public void ChangeCareerCategory_TryToChangeWithNullCareer_ShouldThrow()
-        {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns<Career>(null);
-
-            //act & assert
-            Assert.Throws<ArgumentNullException>(() => CareerService.ChangeCareerCategory(It.IsAny<int>(), It.IsAny<int>()));
-        }
-
-        [Test]
-        public void ChangeCareerCategory_TryToChange_ShouldCallsOnce()
-        {
-            careerRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(new Career { Name = It.IsAny<string>(), Price = It.IsAny<double>(), TradeDuration = It.IsAny<int>() });
-
-            //act
-            CareerService.ChangeCareerCategory(It.IsAny<int>(), It.IsAny<int>());
-
-            //assert
-            careerRepository.Verify(x => x.Update(It.IsAny<Career>()), Times.Once);
         }
 
         [Test]
@@ -184,9 +103,9 @@ namespace WorkBase.Tests.LogicTest
             careerRepository.Setup(x => x.GetAll()).Returns(new List<Career>() { });
 
             //act & assert
-            Assert.IsNotNull(CareerService.GetAllCareers());
+            NUnit.Framework.Assert.IsNotNull(carService.GetAllCareers());
             careerRepository.Verify(x => x.GetAll());
-        }*/
+        }
     }
 }
 
---
+++ WorkBase.Tests/LogicTest/TestExpirienceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkBase.Tests.LogicTest
+{
+    [TestClass]
+    public class TestExpirienceService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
